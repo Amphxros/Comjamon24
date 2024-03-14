1b5ea067b2d41ad7e77efe34eeeb5f925411f793
A. Rubio <ampxrubio@gmail.com>
2024-03-14 22:26:28 +0100

    Dialogos

--- ComJamOn24/Assets/Scripts/RunDialogueOnStart.cs
+++ ComJamOn24/Assets/Scripts/RunDialogueOnStart.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunDialogueOnStart : MonoBehaviour
+public abstract class RunDialogueOnStart : MonoBehaviour
 {
-    [SerializeField]string dialogueScript;
+    [SerializeField] string startingNode;
+    bool talked = false;
     void Start()
     {
-        GameManager.Instance.dialogueSys.StartDialogue(dialogueScript);
+        GameManager.Instance.dialogueSys.StartDialogue(startingNode);
+        Invoke("talk", 1);
     }
 
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (talked && !GameManager.Instance.dialogueSys.IsDialogueRunning)
+        {
+            OnDialogueFinished();
+        }
+    }
+
+    public abstract void OnDialogueFinished();
+    private void talk()
+    {
+        talked = true;
+    }
 }
---
+++ ComJamOn24/Assets/Scripts/RunOnBeginning.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunOnBeginning :RunDialogueOnStart
+{
+    public override void OnDialogueFinished()
+    {
+      //
+    }
+
+  
+}
